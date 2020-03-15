660e53496dc8b9f66c7c2e433394fb12ab892beb
Rikkilit <rikkilitgmail.com>
2020-03-15 20:21:47 +0300

    add function mydiv

--- program1.cpp
+++ program1.cpp
@@ -8,6 +8,10 @@ int main()
 	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
 	cout << MyMul(10, 3) << endl;
+	double MyDiv(double a, double b)
+	{
+		return a / b;
+	}
 	system("pause");
 	return 0;
 }
