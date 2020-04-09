9e7413665ac229d64b8b01645bdfa18ecdd25769
rikkilit <rikkilit@gmail.com>
2020-04-09 20:37:27 +0300

    add function MyPow() version2

--- program1.cpp
+++ program1.cpp
@@ -1,17 +1,15 @@
-//פאיכ program.cpp
+//???? program.cpp
 #include "stdafx.h"
 #include <iostream>
 #include "funcs.h"
 using namespace std;
 int main()
 {
-	cout << MySum(10, 3) << endl;
-	cout << MySub(10, 3) << endl;
-	cout << MyMul(10, 3) << endl;
-	double MyDiv(double a, double b)
-	{
-		return a / b;
-	}
-	system("pause");
-	return 0;
-}
+cout << MySum(10, 3) << endl;
+cout << MySub(10, 3) << endl;
+cout << MyMul(10, 3) << endl;
+cout << MyDiv(10, 3) << endl;
+cout << MyPow(10.5, 3.7) << endl;
+system("pause");
+return 0;
+}
